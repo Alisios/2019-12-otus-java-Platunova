4fa7972c20fae48fa960bfda7e2195acc2b3f667
Alisios <lavii@ya.ru>
2020-03-14 23:15:44 +0300

    Полностью переделанный toJson

--- maven/hw08-json/src/main/java/ru/otus/JSON/DIYgson.java
+++ maven/hw08-json/src/main/java/ru/otus/JSON/DIYgson.java
@@ -1,82 +1,103 @@
 package ru.otus.JSON;
 import com.google.gson.JsonPrimitive;
-
-import javax.json.Json;
-import javax.json.JsonObject;
 import java.lang.reflect.Array;
 import java.lang.reflect.Field;
-import java.util.LinkedHashMap;
-import java.util.Map;
+import java.util.*;
 
-/*Класс, реализцющий формирование json в формате строки и в формате JsonObject  */
+/*Класс, реализцющий формирование json в формате строки по аналогии с Gson() */
 class DIYgson {
-    private static Map<String, Object> map = new LinkedHashMap<>();
 
     String toJson(Object obj)  {
-        var sb = new StringBuilder();
         if (obj == null){
             return "null";
         }
-        else{
-            Field[] fields = obj.getClass().getDeclaredFields();
-            if (fields.length==0)
-                return "{}";
-            sb.append("{");
-            try {
-                for(Field f: fields){
-                    f.setAccessible(true);
-                    Object value = f.get(obj);
-                    if(value == null){
-                        sb.deleteCharAt(sb.length()-1);
-                        sb.deleteCharAt(sb.length()-1);
-                    }
-                    else{
-                        sb.append(f.getName()).append(":");
-                        if (f.getType().toString().contains("TreeMap")){
-                            String str = value.toString().replaceAll("=", ":");
-                            sb.append(str);
-                            map.put(f.getName(),str);
-                        }
-                        else if(f.getType().toString().contains("class java.lang.String")&& value.equals("")){
-                            map.put(f.getName(),"");
-                            sb.append(new JsonPrimitive(""));
-                        }
-                        else if(f.getType().toString().contains("[")){
-                                StringBuilder str = new StringBuilder();
-                                sb.append("[");
-                                str.append("[");
-                                for (int i=0; i < Array.getLength(value)-1; i++){
-                                    sb.append(Array.get(value, i)).append(",");
-                                    str.append(Array.get(value, i)).append(",");
-                                }
-                                str.append(Array.get(value, Array.getLength(value) - 1)).append("]");
-                                sb.append(Array.get(value, Array.getLength(value) - 1)).append("]");
-                                map.put(f.getName(),str.toString());
-                        }
-                        else{
-                            sb.append(value.toString());
-                            map.put(f.getName(),value.toString());
-                        }
-                    }
-                    sb.append(", ");
+        return chooseMethodToJson(obj);
+    }
+
+    /** выбор метода перевода объекта в формат Json в зависимости от типа объекта**/
+    private String chooseMethodToJson(Object obj) {
+        if (obj.getClass().getTypeName().contains("[]"))
+            return arrayClassToJson(obj);
+        else if (obj.getClass().getTypeName().contains("java.lang."))
+            return primitiveClassToJson(obj);
+        else if (obj.getClass().getTypeName().contains("java.util."))
+            return collectionClassToJson(obj);
+        else
+            return objectClassToJson(obj);
+    }
+
+    /**перевод объекта в формат Json, если его типа нет в стандартной библиотеке Java**/
+    private String objectClassToJson(Object obj){
+        var sb = new StringBuilder();
+        Field[] fields = obj.getClass().getDeclaredFields();
+        if (fields.length==0)
+            return "{}";
+        sb.append("{");
+        try {
+            for(Field f: fields){
+                f.setAccessible(true);
+                Object value = f.get(obj);
+                if(value == null)
+                    sb.deleteCharAt(sb.length()-1);
+                else{
+                    sb.append(primitiveClassToJson(f.getName())).append(":");
+                    sb.append(chooseMethodToJson(value));
                 }
+                sb.append(",");
             }
-            catch (Exception e){
-                System.out.println(e.getCause()+" in " + this.getClass().getName());
-            }
-            sb.deleteCharAt(sb.length()-1);
-            sb.deleteCharAt(sb.length()-1);
-            sb.append("}");
         }
+        catch (Exception e){
+            System.out.println(e.getCause()+" in " + this.getClass().getName());
+        }
+        sb.deleteCharAt(sb.length()-1);
+        sb.append("}");
         return sb.toString();
     }
 
-    Map<String , Object> getMap(){
-        return map;
+    /**перевод объекта в формат Json, если его тип это примитив или строка**/
+    private String primitiveClassToJson(Object obj){
+        switch (obj.getClass().getTypeName()) {
+
+            case "java.lang.Character":
+                return String.valueOf(new JsonPrimitive((Character) obj));
+
+            case "java.lang.String":
+                return String.valueOf(new JsonPrimitive((String) obj));
+
+            default:
+                return obj.toString();
+        }
     }
 
-    JsonObject create() {
-        return Json.createObjectBuilder(map).build();
+    /**перевод объекта в формат Json, если его тип это коллекция**/
+    private String collectionClassToJson(Object obj){
+        var sb = new StringBuilder();
+        if (obj.getClass().getTypeName().contains("TreeMap")){
+            {
+                String str2 = obj.toString().replace("{", "{\"");
+                String str = str2.replaceAll("=", "\":");
+                String str1 = str.replaceAll("],", "],\"");
+                return sb.append(str1.replaceAll(" ", "")).toString();
+            }
+        }
+        ArrayList a = new ArrayList();
+        sb.append("[");
+        a.addAll((Collection) obj);
+        for (int i=0; i<a.size()-1; i++){
+            sb.append(primitiveClassToJson(a.get(i))).append(",");
+        }
+        sb.append(primitiveClassToJson(a.get(a.size() - 1)));
+        sb.append("]");
+        return sb.toString();
     }
 
+    /**перевод объекта в формат Json, если его тип это массив**/
+    private String arrayClassToJson(Object obj){
+        var sb = new StringBuilder();
+        sb.append("[");
+        for (int i=0; i < Array.getLength(obj)-1; i++){
+            sb.append(primitiveClassToJson(Array.get(obj, i))).append(",");
+        }
+        return sb.append(primitiveClassToJson(Array.get(obj, Array.getLength(obj) - 1))).append("]").toString();
+    }
 }
--- maven/hw08-json/src/main/java/ru/otus/JSON/JsonWork.java
+++ maven/hw08-json/src/main/java/ru/otus/JSON/JsonWork.java
@@ -10,32 +10,8 @@ import java.io.*;
 
 public class JsonWork {
     public static void main(String[] args) {
-        String nameOfFile = "testOfJson.txt";
-
-        HWObject obj = new HWObject();
-        var gson = new DIYgson();
-        String json = gson.toJson(obj);
-        JsonObject jsonObject = gson.create();
-        System.out.println(jsonObject);
-        System.out.println();
-
-        //весь код ниже для тестирования и демонстрации
-
-        try {
-            JsonWriter jsonWriter = Json.createWriter(new FileOutputStream(new File(nameOfFile)));
-            jsonWriter.writeObject(jsonObject);
-            jsonWriter.close();
-
-            JsonReader jsonReader = Json.createReader(new FileInputStream(nameOfFile));
-            JsonObject rootJSON = jsonReader.readObject();
-            System.out.println("Are JsonObjects equal? " + rootJSON.equals(jsonObject));
-            jsonReader.close();
-        }
-        catch (Exception e){
-            System.out.println(e.getCause()+" in main() of JsonWork");
-        }
-
-        HWObject obj3 = new Gson().fromJson(json, HWObject.class);
-        System.out.println("Is BeforeGson equal to AfterGson? " + obj.equals(obj3));
+        var serializer = new DIYgson();
+        Gson gson = new Gson();
+        System.out.println("Are two gsons equals: ? " +  gson.toJson(new HWObject()).equals(serializer.toJson(new HWObject())));
     }
 }
